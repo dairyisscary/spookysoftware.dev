ae8a7cffd1013a1656bba856552e286ec07bee2e
Eric Butler <eric.butler@notarize.com>
2020-02-09 11:15:59 -0800

    Add Some Content-Type Headers

--- www/bin/upload-to-s3.sh
+++ www/bin/upload-to-s3.sh
@@ -5,11 +5,11 @@ BROWSER_EXPIRES="public, max-age=0, s-maxage=$ONE_YEAR, must-revalidate"
 BROWSER_IMMUTABLE="public, max-age=$ONE_YEAR, immutable"
 
 # HTML and Page Data that can't be (browser) cached
-aws s3 cp --recursive --metadata-directive REPLACE --cache-control "$BROWSER_EXPIRES" --exclude "*" --include "*.html" public "s3://$BUCKET_NAME"
+aws s3 cp --recursive --metadata-directive REPLACE --content-type "text/html; charset=utf-8" --cache-control "$BROWSER_EXPIRES" --exclude "*" --include "*.html" public "s3://$BUCKET_NAME"
 aws s3 cp --recursive --metadata-directive REPLACE --cache-control "$BROWSER_EXPIRES" public/page-data "s3://$BUCKET_NAME/page-data"
 aws s3 cp --metadata-directive REPLACE --cache-control "$BROWSER_EXPIRES" public/chunk-map.json "s3://$BUCKET_NAME"
 aws s3 cp --metadata-directive REPLACE --cache-control "$BROWSER_EXPIRES" public/webpack.stats.json "s3://$BUCKET_NAME"
-aws s3 cp --metadata-directive REPLACE --cache-control "$BROWSER_EXPIRES" public/rss.xml "s3://$BUCKET_NAME"
+aws s3 cp --metadata-directive REPLACE --content-type "application/rss+xml" --cache-control "$BROWSER_EXPIRES" public/rss.xml "s3://$BUCKET_NAME"
 
 # Static and JS/CSS that can be cached forever
 aws s3 cp --recursive --metadata-directive REPLACE --cache-control "$BROWSER_IMMUTABLE" public/static "s3://$BUCKET_NAME/static"
