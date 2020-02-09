108e3dc9c5c7082a7781b5228440b5e3fad0d133
Eric Butler <eric@kimbutler.xyz>
2020-02-08 18:36:41 -0800

    Add Grace's Edits to the About Page

--- www/src/pages/about.tsx
+++ www/src/pages/about.tsx
@@ -17,35 +17,48 @@ function About() {
       <PageContent>
         <p>
           Circa 2007, I stumbled upon the great wasteland known as{" "}
-          <NewTabLink href="https://myspace.com/">MySpace</NewTabLink>. I wrote
-          my first lines of CSS trying to make the background of my profile a
-          baller shade of{" "}
+          <NewTabLink href="https://myspace.com/">MySpace.com</NewTabLink>. I
+          wrote my first lines of CSS trying to make the background of my
+          profile a baller shade of{" "}
           <span title={FUCHSIA} style={{ color: FUCHSIA }}>
             Fuchsia
           </span>
-          . Needless to say, I blinked first, and {yearsSince2007} years later,
-          I'm still trying to get that same high.
+          . As an adult, card-carrying Software Engineer {yearsSince2007} years
+          later, I'm still trying to get that same high.
         </p>
         <p>
-          Somewhere along the way, I studied Philosophy in college, which more
-          than qualifies my disdain for your shitty startup. I still cannot
-          bring myself to use the “it’s all Greek to me” joke because, well,
-          most of the time it really <em>is</em> all Greek to me.
-        </p>
-        <p>
-          I have worked for the last {yearsSince2014} years at a{" "}
-          <NewTabLink href="https://www.shoobx.com/">few</NewTabLink> software{" "}
+          I have worked for the last {yearsSince2014} years at a few web-SaaS{" "}
+          <NewTabLink href="https://www.shoobx.com/">software</NewTabLink>{" "}
           <NewTabLink href="https://www.notarize.com/">startups</NewTabLink>{" "}
           writing code that only just barely makes the thing work. In my
-          defense, isn’t that really what engineering is? Making the worst and
-          cheapest solution to a problem that still works?
+          defense, isn’t that really what engineering is -- making the cheapest
+          solution that still works?
         </p>
         <p>
           These days I have been thinking more about educational content for the
-          working engineer, with particular emphasis on whatever thing is
-          currently upsetting me. This blog is that outlet. Here is to hoping
-          it’s useful to you.
+          working practitioner and what it means to be an engineer today.
+          Hopefully this blog gives me a chance to occasionally dust off my
+          Philosophy degree and share some hard-earned lessons from my
+          experience in the industry.
         </p>
+        <h3>Projects</h3>
+        <ul>
+          <li>
+            <a href="https://github.com/notarize/qlc">QLC</a> (A GraphQL Code
+            Generation Tool)
+          </li>
+          <li>
+            <a href="https://www.youtube.com/playlist?list=PLEsBiOG9ZllOuN6RoDH8DAtbMVuzPDwyb">
+              For Your Reference Video Series
+            </a>{" "}
+            (Rust Beginner Tutorial)
+          </li>
+          <li>
+            <a href="https://github.com/dairyisscary/spookysoftware.dev">
+              This Website's Source Code
+            </a>
+          </li>
+        </ul>
       </PageContent>
     </Layout>
   );
