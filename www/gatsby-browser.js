8fdfda566e3024b4e6296bb719c9128291944bed
Eric Butler <eric@kimbutler.xyz>
2020-05-16 12:40:37 -0700

    Update Fathom Tracking

--- www/gatsby-browser.js
+++ www/gatsby-browser.js
@@ -1,12 +1,2 @@
 import "@/global.scss";
 import "prismjs/themes/prism-tomorrow.css";
-
-export const onRouteUpdate = ({ location }) => {
-  if (
-    process.env.NODE_ENV === "production" &&
-    typeof window.fathom === "function" &&
-    location.hostname === "www.spookysoftware.dev"
-  ) {
-    window.fathom("trackPageview");
-  }
-};
--- www/gatsby-ssr.js
+++ www/gatsby-ssr.js
@@ -1,28 +1,14 @@
 import React from "react";
 
 function getFathomScriptTags() {
-  const fathomInlineContent = `
-    (function(f,a,t,h){
-      a[h]=a[h]||function(){
-        (a[h].q=a[h].q||[]).push(arguments)
-      };
-    })(document,window,null,"fathom");
-    fathom("set", "siteId", "IAXGRDBV");
-    if (window.location.hostname === "www.spookysoftware.dev")
-      fathom("trackPageview");
-  `;
   return [
-    <script
-      key="fathom-inline-js"
-      type="text/javascript"
-      dangerouslySetInnerHTML={{ __html: fathomInlineContent }}
-    />,
     <script
       key="fathom-remote-js"
       type="text/javascript"
-      async
-      src="https://cdn.usefathom.com/tracker.js"
-      id="fathom-script"
+      src="https://sheep.spookysoftware.dev/script.js"
+      spa="auto"
+      site="IAXGRDBV"
+      defer
     />,
   ];
 }
