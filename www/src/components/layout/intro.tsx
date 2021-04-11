23feb0d20dfaae154b71d95c587728f41050e736
Eric Kim-Butler <eric.butler@notarize.com>
2021-04-11 11:41:51 -0400

    Upgrade www Deps

--- www/src/components/layout/intro.tsx
+++ www/src/components/layout/intro.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-import Styles from "./intro.module.scss";
+import { main } from "./intro.module.scss";
 
 function Intro() {
   return (
     <div className="mb-16">
-      <h4 className={`${Styles.main} font-serif text-highlight text-3xl`}>
+      <h4 className={`${main} font-serif text-highlight text-3xl`}>
         Hi, I'm Eric. I'm a software engineer, and this is where I write my
         complaints.
       </h4>
--- www/src/global.d.ts
+++ www/src/global.d.ts
@@ -2,8 +2,3 @@ declare module "*.png" {
   const src: string;
   export default src;
 }
-
-declare module "*.module.scss" {
-  const styles: { [className: string]: string };
-  export default styles;
-}
